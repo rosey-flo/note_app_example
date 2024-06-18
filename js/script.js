cceebb1851d9088613a26bde0a98cf3434b434a3
rosey-flo <emilyflo2012@yahoo.com>
2024-06-18 10:23:10 -0400

    finished console basics

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Javascript works with or has a set of different values
+//A word or multiple words wrapped in quotes is called a string value type
+// number is called a Number value type
+var firstName = 'Emily';
+
+console.log(3);
+
+console.log(5 - 3 * 2);
+
+console.log(firstName)
