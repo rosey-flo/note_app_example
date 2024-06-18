8e2d403127daaffdda41714a3b5c219329c7278d
rosey-flo <emilyflo2012@yahoo.com>
2024-06-18 13:31:03 -0400

    logical operators

--- js/script.js
+++ js/script.js
@@ -4,70 +4,36 @@
 var firstName = 'Emily'; //string value type(legacy term "var" is being used)
 var lastName = 'Flores';
 var age = 44; //Number value type
-var isADev = true; //Boolean value type
-var un = undefined; // Undefined is a calue type that JS will give you when there is no declared or assigned value
-var n = null; // Programmer's representation of nothing - we may use this as a placeholder until a value is recieved
-var notANumber = NaN; // Not a number value tye - this is ptovided when you try to process a mathmathical equation and the result is not a valid number
-
-var nothing;
-
-//console.log(isADev); // Using a variable within your code is called a reference
-
-// age = age + 1; //this is referencing the already exisiting value type above 'age' and edits it
-
-//age += 5; //this is an increment operator =+ to increase age by a numerical amount that we set, in this case 5
-//age = age + 1;
-age ++; // increases a variable by 1
-
-//console.log(age);
-
-age --;//decreases your number value by 1
-
-//console.log(age);
-
-//age -= 4;
-
-//console.log(age);
-
-//console.log(firstName + ' Tadlock') //combines but does not reassign
-
-//firstName + ' Tadlock'; //as a combinator this will not fuse the names together on console
-
-//console.log(firstName); //does not work to add tadlock
-
-//console.log(firstName + ' ' + lastName);
-
-var fullName = firstName + ' ' + lastName;
-
-console.log(fullName); //This a great way to store variabes to create a !descriptive! outcome
-
 var names = ['john', 'anne', 'bob', 'jane']; // Array
 
-//console.log(names[2]);//by putting the [2] in the variable area, you care calling the 2 indexed value of the array which would be bob, aka they art with 0, 1, 2...
+//Comparison Operators --- to get Boolean results based on  the results of your comparisom
+//console.log(age < 30);
 
-//console.log(names.length); // gives you the number of items in the referenced array
+//console.log(age == 30);
+//console.log (age == '44'); //== will only check values so even if you put a number value in the string '' marks, it will still read as a number value
+//console.log(age ==='44'); // === this is a strict quality operator that check both value and type, therefore the 'string' marks, makes it false
 
-//console.log(names[names.length-1])
+//console.log(age >= 40); //greater than or equal to
+//console.log(age <= 40); //less than or equal to
 
-//names[1] = 'Kim';
+//console.log(age != 44); //!= means NOT so not equal to 44 would be false
+//console.log(age !== 44); //!== means NOT so not equal to value 44 AND type
 
-//console.log(names[1])
+//Logical Operstors - Return a value on either the left side or right side of the operator
+//The value that is equated is based on truth (truthy vs falsey)
 
-//var ages = [40, 24, 54, 12]
-//ages[2] += 10;
-//console.log(ages);
+//console.log(0 || null || '' || -5 || undefined); //If anyone of these is true, then perfom some action
 
-//names.push('susan'); //this pushes or add a name to your array
-//console.log(names)
+//falsey values: 0, null, undefined, '' (empty string), NaN, false
+//if neither is truthy it will return the last value in the chain or the first truthy value
 
-//names.unshift('Eli'); //this will add a name to the beginning of an array
-//console.log(names);
+//console.log(1 && age && '' && true);//stops at the first falsey --- returns the first falsey, in this case ''
+//console.log(1 && age && 100 && 90); //will fall back to last value if all are true
 
-//names.pop(); //removes the last item in a refrenced array
-//console.log(names);
+//console.log(!50); //if you put a truthy value, it will output the opposite sice we are putting NOT!, or vice versa
+//console.log(!'');
 
-//names.shift(); //removes the first item in a refrenced array
-//console.log(names);
+//progrma is a list of AuthenticatorAssertionResponse, to mke a file of code to perfom some AuthenticatorAssertionResponse,
+//those actions are determined by the systems checks ans balances via the conditional statements
 
-names.splice(3,1); //this can remove a middle value in an array, the first number is the index and the second number is the amount you are reomving
-console.log(names);
+
