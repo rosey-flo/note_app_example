a9a3ce0b29eeda888792c95b816bcee1bf522564
rosey-flo <emilyflo2012@yahoo.com>
2024-06-21 12:39:48 -0400

    finished calculator practice activity

--- js/script.js
+++ js/script.js
@@ -1,65 +1,126 @@
+//psuedocoding is way to start the layout of the code
+
+/* 
+Create a function that takes 3 arguments
+-Thie first and second argument will be a number
+-The third argument will be a mathmatical operator (+, -, *, /)
+
+ExampleL if the operator is a "+" then you will add the two numbers together and console log the sum
+*/
+
+//Call your function 4 times with different numbers and operators to confirm if it is working correctly
 
 
 /*
-//functions and their codes are hoisted to the top and can be called before being declared
 
-myFirstFunc();
-
-//==============================
-
-function myFirstFunc () { //() holds a pararemeter list, and the words after fucntion will be your reference name to the code block
-   console.log('function has been called');
+function calculator(num1, num2, operator) {
+   if (typeof num1 === 'number' && typeof num2 === 'number' && typeof operator === 'string') {
+      if (operator === '+') {
+         console.log(num1 + num2);
+      }
+      else if (operator === '-') {
+         console.log(num1 - num2);
+      }
+      else if (operator === '*') {
+         console.log(num1 * num2);
+      }
+      else if (operator === '/') {
+         console.log(num1 / num2);
+      }
+      else {
+         console.log('invalid answer')
+      }
+   }
+   else {
+      if (typeof num1 !== 'number') {
+         console.log("The first input is not a numeric value")
+      }
+      else if (typeof num2 !== 'number') {
+         console.log("The second input is not a numeric value")
+      }
+      else if (typeof operator !== 'string') {
+         console.log("The third input is not a string value")
+      }
+         
+   }
 }
 
-//this is declaration above
-//==============================
-//This is calling it below
 
-myFirstFunc();
-
-//This is meant to be used a a repeatable code !!
+calculator ("20", "10", '+')
 
 */
 
 
+
+
+
+
 /*
+
+function calculator(num1, num2, operator) {
+   switch (operator) {
+      case '+':
+         console.log(num1 + num2);
+         break;
+      case '-':
+         console.log(num1 - num2);
+         break;
+      case '*':
+         console.log (num1 * num2)
+         break;
+      case '/':
+         console.log (num1 / num2)
+         break;
+      default:
+         console.log('you must provide a valid operator');
+   }
+}
+
+
+calculator(20, 10, '+')
+calculator(20, 10, '-')
+calculator(20, 10, '*')
+calculator(20, 10, '/')
+
+
+
+
+//'return' is the function's anwer
+
+
 function add(num1, num2) {
-   console.log(num1 + num2); //this will be the action you want performed with your refrenced, delclared values
-}
+   return 'something else'
+   }
+   console.log (add());
 
-add(5, 10); // pthis is where you will pass your arguments and are comma separated, and will then be referenced in the () as 'parameter'
-// this can then be used to get the sum of all different numbers
-
-add(20, 27);
+//===============
+   add(5,10);
+   console.log ('something after our function call')
+   
 */
 
-
-
-//=====================
-
-/*
-function printValues (num, boolean, name, another) {
-   console.log(another); //this will be undefined since a 4th value is not being declared
-}
-printValues(10, 'Allen', false);
-//the print or output will line up exactly with the value order you put in, therefore you will get an outout if "allen" when calling 'boolean for example
-*/
-//======================
-
-
-function printNames(names) { //the () are the refrence name for the array listed below
-   for (const name of names) { //this for / of reference is best used in times liike this becasue we are calling each name from a parent list of names
-   console.log(name);
-}
+function calculator(num1, num2, operator) {
+   switch (operator) {
+      case '+':
+         return num1 + num2;
+      case '-':
+         return num1 - num2;
+      case '*':
+         return num1 * num2;
+      case '/':
+         return num1 / num2;
+      default:
+         return 'you must provide a valid operator';
+   }
 }
 
-printNames (['Bob', 'Sally', 'Jimmy']) //array will be used as a single parameter in the function
 
-/*
-printNames ('hello'); // this would print out the character of the single string we had, aka name = the characters
+const sum = calculator(20, 10, '+');
+console.log (sum);
+const kevin = 5;
+console.log (typeof(kevin));
 
-printNames ('jake','justin'); //This would also be read as an characters bc it is not within the [] brackets
-
-printNames (113); //strings and arrays are the only ones that can processed in this manner, numbers do not
-*/
+calculator(20, 10, '-')
+calculator(20, 10, '*')
+calculator(20, 10, '/')
 
