6c9bb531392c0a45817c47b224ca0bba5101f3d2
rosey-flo <emilyflo2012@yahoo.com>
2024-06-25 14:23:50 -0400

    finished note app example

--- js/script.js
+++ js/script.js
@@ -1,126 +1,109 @@
-//psuedocoding is way to start the layout of the code
+//target the form 
+const noteForm = document.querySelector('#note-form');
+//form "entry" E #1 for formE1, storying the query selecter in that variable
 
-/* 
-Create a function that takes 3 arguments
--Thie first and second argument will be a number
--The third argument will be a mathmatical operator (+, -, *, /)
+//target the note header h3 
+//to target h3 header for the note
+const noteHeader = document.querySelector('.note-header');
 
-ExampleL if the operator is a "+" then you will add the two numbers together and console log the sum
-*/
+function getNotes() {
+    const rawNoteArray = localStorage.getItem('notes');
+    const notes = JSON.parse(localStorage.getItem('notes')) || []; //converts to array
+    //push the  raw notes object into the notes array
+    return notes;
+}
 
-//Call your function 4 times with different numbers and operators to confirm if it is working correctly
 
 
+
+//a function that is called when the form is submitted
+function createNote(eventObj) {
+    eventObj.preventDefault();
+
+    //grab the note input 
+    const noteInput = document.querySelector('#note-input');
+    //get the value what they typed into the box
+    const noteText = noteInput.value;
+    //create a date value index
+    const dateObj = new Date();
+    //get the month from the date object
+    const month = dateObj.getMonth() + 1;
+    //get date from date object
+    const date = dateObj.getDate();
+    //get the year from the date object
+    const year = dateObj.getFullYear();
+    //conbine all of those values into a string that looks like this 06/24/2024
+
+
+    //const dateStr = month + '/' + date + '/' + year;
+    //OR
+    const dateStr = `${month}/${date}/${year}`;
+    //create an ibject value with the note text and the current date
+    //the object should have two properties - text and date
+    //KET VALUE PAIR
+    const entryStr = {
+        text: noteText,
+        date: dateStr
+    }
+    /*
+    //pull the old data from the database (localstorage) or an empy array if no previous data has been stored
+    const rawNoteArray = localStorage.getItem('notes');
+    const notes = JSON.parse(localStorage.getItem('notes')) || []; //converts to array
+    */
+    const notes = getNotes();
+
+    //push the  raw notes object into the notes array
+    notes.push(entryStr);
+
+    localStorage.getItem('notes', JSON.stringify(notes));
+
+    const jsonArray = JSON.stringify(notes);
+    localStorage.setItem('notes', jsonArray);
+
+    outputNotes();
+    noteInput.value = '';
+
+}
+
+
+
+function outputNotes() {
+    const notes = getNotes();
+    const container = document.querySelector('.container');
+    //this container allows us to interact w/ the main section container in our index
+
+
+    if (notes.length) {
+        noteHeader.innerText = 'Your Notes:'
+    }
+
+    container.innerHTML = '';
+//This will clear the container before we begin the loop 
+
+
+
+    for (const entryStr of notes) {
+        //Target our main container element
+        container.insertAdjacentHTML('beforeend', `
+        <article class="note">
+            <p class="note-text">${entryStr.text}</p>
+            <p class="note-date">Created: ${entryStr.date}</p>
+        </article>
+        `);
+
+    }
+
+
+}
+
+
+outputNotes();
+noteForm.addEventListener('submit', createNote);
 /*
 
-function calculator(num1, num2, operator) {
-   if (typeof num1 === 'number' && typeof num2 === 'number' && typeof operator === 'string') {
-      if (operator === '+') {
-         console.log(num1 + num2);
-      }
-      else if (operator === '-') {
-         console.log(num1 - num2);
-      }
-      else if (operator === '*') {
-         console.log(num1 * num2);
-      }
-      else if (operator === '/') {
-         console.log(num1 / num2);
-      }
-      else {
-         console.log('invalid answer')
-      }
-   }
-   else {
-      if (typeof num1 !== 'number') {
-         console.log("The first input is not a numeric value")
-      }
-      else if (typeof num2 !== 'number') {
-         console.log("The second input is not a numeric value")
-      }
-      else if (typeof operator !== 'string') {
-         console.log("The third input is not a string value")
-      }
-         
-   }
-}
-
-
-calculator ("20", "10", '+')
-
+//set up ny necessary event listeners that need to be listening when the page loads
+const rawNoteArray = localStorage.getItem('notes');
+const parsed = JSON.parse(rawNoteArray);
+console.log(rawNoteArray);
 */
-
-
-
-
-
-
-/*
-
-function calculator(num1, num2, operator) {
-   switch (operator) {
-      case '+':
-         console.log(num1 + num2);
-         break;
-      case '-':
-         console.log(num1 - num2);
-         break;
-      case '*':
-         console.log (num1 * num2)
-         break;
-      case '/':
-         console.log (num1 / num2)
-         break;
-      default:
-         console.log('you must provide a valid operator');
-   }
-}
-
-
-calculator(20, 10, '+')
-calculator(20, 10, '-')
-calculator(20, 10, '*')
-calculator(20, 10, '/')
-
-
-
-
-//'return' is the function's anwer
-
-
-function add(num1, num2) {
-   return 'something else'
-   }
-   console.log (add());
-
-//===============
-   add(5,10);
-   console.log ('something after our function call')
-   
-*/
-
-function calculator(num1, num2, operator) {
-   switch (operator) {
-      case '+':
-         return num1 + num2;
-      case '-':
-         return num1 - num2;
-      case '*':
-         return num1 * num2;
-      case '/':
-         return num1 / num2;
-      default:
-         return 'you must provide a valid operator';
-   }
-}
-
-
-const sum = calculator(20, 10, '+');
-console.log (sum);
-const kevin = 5;
-console.log (typeof(kevin));
-
-calculator(20, 10, '-')
-calculator(20, 10, '*')
-calculator(20, 10, '/')
 
